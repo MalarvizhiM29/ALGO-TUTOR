4378921ebc4a6210b5d178e97cf157598a01c400
MALARVIZHI M <Malarm2004@gmail.com>
2023-03-11 23:34:16 +0530

    Question9

---
+++ Homework-1/gcdpair.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class gcdpair {
+    public static void main(String[] args) {
+        int[] arr = { 2, 2, 2, 2, 8, 2, 2, 2, 10 };
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+        for (int i : arr) {
+            set.add(i);
+        }
+        System.out.println(set);
+    }
+}
